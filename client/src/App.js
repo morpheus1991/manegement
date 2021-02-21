4f7e04c900d1c5a8fcf117bf6159ee1bccf1aaf9
Mori <chohyuk072@gmail.com>
2021-02-22 00:58:40 +0900

    Add customer API Loding View

--- client/src/App.js
+++ client/src/App.js
@@ -7,51 +7,52 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 2,
     overflowX: "auto",
   },
   table: {
     minWidth: 1080,
   },
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
 });
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "태완이",
-    birthday: 961222,
-    gender: "남자",
-    job: "퍼블리셔",
-    lover: "solo",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "규형이",
-    birthday: 901023,
-    gender: "남자",
-    job: "뒤틀린듀오의퍼블리셔",
-    lover: "커플",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "츠키",
-    birthday: 210,
-    gender: "남자",
-    job: "귀여움",
-    lover: "고양이",
-  },
-];
 class App extends Component {
+  state = {
+    customers: "",
+    completed: 0,
+  };
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    console.log("실행");
+    console.log(this.timer);
+    this.callApi()
+      .then((res) =>
+        this.setState({
+          customers: res,
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({
+      completed: completed >= 100 ? 0 : completed + 1,
+    });
+  };
   render() {
     const { classes } = this.props;
-    console.log(classes);
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -65,21 +66,33 @@ class App extends Component {
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
+
           <TableBody>
-            {customers.map((custormer) => {
-              return (
-                <Customer
-                  key={custormer.id}
-                  name={custormer.name}
-                  birthday={custormer.birthday}
-                  gender={custormer.gender}
-                  job={custormer.job}
-                  id={custormer.id}
-                  image={custormer.image}
-                  lover={custormer.lover}
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress
+                  className={classes.progress}
+                  variant="determinate"
+                  value={this.state.completed}
                 />
-              );
-            })}
+              </TableCell>
+            </TableRow>
+            {this.state.customers
+              ? this.state.customers.map((custormer) => {
+                  return (
+                    <Customer
+                      key={custormer.id}
+                      name={custormer.name}
+                      birthday={custormer.birthday}
+                      gender={custormer.gender}
+                      job={custormer.job}
+                      id={custormer.id}
+                      image={custormer.image}
+                      lover={custormer.lover}
+                    />
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,36 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send("Hello Express!");
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "태완이",
+      birthday: 961222,
+      gender: "남자",
+      job: "퍼블리셔",
+      lover: "solo",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "규형이",
+      birthday: 901023,
+      gender: "남자",
+      job: "뒤틀린듀오의퍼블리셔",
+      lover: "커플",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "츠키",
+      birthday: 210,
+      gender: "남자",
+      job: "귀여움",
+      lover: "고양이",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
